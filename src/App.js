919362ce04d507ba32ac7c40ec250e52c69f1802
irhamjaya <hino_weleh@hotmail.com>
2019-01-18 13:46:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="img//" alt="Card image cap"/>
+      <div class="card-body">
+        <p class="card-text">why people alaways come n go?</p>
+        <br></br>
+        <p class="card-text">because him/her not alone</p>
       </div>
+    </div>
     );
   }
 }
